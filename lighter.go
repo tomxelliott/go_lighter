cf26e9b791580f7b6dfd4340180e0222049dfc9f
Tom Elliott <tomxelliott@hotmail.co.uk>
2018-01-14 13:35:43 +0000

    Update lighter.go

--- lighter.go
+++ lighter.go
@@ -17,22 +17,24 @@ var noOfColours int
 
 func main() {
 	if len(os.Args) > 1 {
-		fmt.Println("This program only takes one argument!")
+		fmt.Println("This program does not take any additional arguments!")
 	} else {
 		reader := bufio.NewReader(os.Stdin)
 		fmt.Println("Please enter how many lights you want:")
-
 		valid := false
 		for valid == false {
 			text, _ := reader.ReadString('\n')
-
 			lightNo := strings.TrimSpace(text)
 
-			if _, err := strconv.Atoi(lightNo); err != nil {
+			if ln, err := strconv.Atoi(lightNo); err != nil {
 				fmt.Println(err.Error())
-				fmt.Println("Please enter a valid number of lights:")
+				fmt.Println("WARNING: Please enter a valid number of lights:")
 			} else {
-				valid = true
+				if ln <= 0 {
+					fmt.Println("WARNING! Please enter a positive number greater than Zero:")
+				} else {
+					valid = true
+				}
 			}
 
 			if valid == true {
@@ -49,12 +51,12 @@ func main() {
 
 			colourNo := strings.TrimSpace(text)
 
-			if _, err := strconv.Atoi(colourNo); err != nil {
+			if cn, err := strconv.Atoi(colourNo); err != nil {
 				fmt.Println(err.Error())
 				fmt.Println("Please enter a valid number of colours:")
-			} else if colourInput, _ := strconv.Atoi(colourNo); colourInput == 0 {
+			} else if cn <= 0 {
 				fmt.Println("Please enter a number greater than ZERO (0) for your number of colours:")
-			} else if colourInput, _ := strconv.Atoi(colourNo); colourInput > noOfLights {
+			} else if cn > noOfLights {
 				noOfColours = noOfLights
 				fmt.Println("Warning!")
 				fmt.Println("The number of colours exceeded the number of lights.")
